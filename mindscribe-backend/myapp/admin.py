f93082c6c6b182a8c7396c41df879797a247ce28
paulmcgonigle67 <s00210326@atu.ie>
2023-11-04 17:14:23 +0000

    First commit with frontend and backend and gitfiles

---
+++ mindscribe-backend/myapp/admin.py
@@ -0,0 +1,54 @@
+from django.contrib import admin
+from .models import User, JournalEntry, Emotion, MoodEntry, UserImprovement, Insight
+
+
+# Define inline admin classes for related models
+class EmotionInline(admin.TabularInline):
+    model = Emotion
+    extra = 1  # Specifies how many extra forms to display
+
+
+class InsightInline(admin.StackedInline):
+    model = Insight
+    extra = 0  # You can set this to 0 if you don't want extra empty forms
+
+
+# Define the admin classes for each model
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "is_staff")
+    search_fields = ("username", "email")
+
+
+class JournalEntryAdmin(admin.ModelAdmin):
+    list_display = ("entryID", "user", "timestamp")
+    list_filter = ("timestamp",)
+    inlines = [EmotionInline, InsightInline]
+
+
+class EmotionAdmin(admin.ModelAdmin):
+    list_display = ("emotionID", "entry", "emotionType")
+    list_filter = ("emotionType",)
+
+
+class MoodEntryAdmin(admin.ModelAdmin):
+    list_display = ("moodEntryID", "user", "timestamp", "moodRating")
+    list_filter = ("moodRating", "timestamp")
+
+
+class UserImprovementAdmin(admin.ModelAdmin):
+    list_display = ("tipId", "user", "timestamp", "tipType")
+    list_filter = ("tipType", "isActive")
+
+
+class InsightAdmin(admin.ModelAdmin):
+    list_display = ("insightID", "entry", "sentimentScore", "timestamp")
+    list_filter = ("sentimentScore",)
+
+
+# Register each model with its respective admin class
+admin.site.register(User, UserAdmin)
+admin.site.register(JournalEntry, JournalEntryAdmin)
+admin.site.register(Emotion, EmotionAdmin)
+admin.site.register(MoodEntry, MoodEntryAdmin)
+admin.site.register(UserImprovement, UserImprovementAdmin)
+admin.site.register(Insight, InsightAdmin)
---
+++ mindscribe-backend/myapp/models.py
@@ -0,0 +1,105 @@
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.utils.translation import gettext_lazy as _
+
+
+# inherits AbstractUser --djangos built-in user model providing authentication fields
+class User(AbstractUser):
+    # Additional fields go here
+
+    # overriding the groups and user_permissions from 'AbstarctUser' to resolve conflicts
+
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=_("groups"),
+        blank=True,
+        help_text=_(
+            "The groups this user belongs to. A user will get all permissions granted to each of their groups."
+        ),
+        related_name="custom_user_groups",  # Unique related_name
+        related_query_name="user",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=_("user permissions"),
+        blank=True,
+        help_text=_("Specific permissions for this user."),
+        related_name="custom_user_permissions",  # Unique related_name
+        related_query_name="user",
+    )
+    # if no additional fields are added , use pass
+    pass
+
+
+# Model for Journal Entries
+class JournalEntry(models.Model):
+    entryID = models.AutoField(primary_key=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="journal_entries"
+    )
+    timestamp = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return f"Journal Entry {self.entryID} by {self.user.username}"
+
+
+# Model for Emotions detected in Journal Entries
+class Emotion(models.Model):
+    emotionID = models.AutoField(primary_key=True)
+    entry = models.ForeignKey(
+        JournalEntry, on_delete=models.CASCADE, related_name="emotions"
+    )
+    emotionType = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Emotion {self.emotionType} for Entry {self.entry.entryID}"
+
+
+# Model for Mood Entries
+class MoodEntry(models.Model):
+    moodEntryID = models.AutoField(primary_key=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="mood_entries"
+    )
+    timestamp = models.DateTimeField(auto_now_add=True)
+    moodRating = models.IntegerField()
+    note = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Mood Entry {self.moodEntryID} by {self.user.username}"
+
+
+# Model for User Improvements like tips, recommendations, etc.
+class UserImprovement(models.Model):
+    tipId = models.AutoField(primary_key=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_improvements"
+    )
+    timestamp = models.DateTimeField(auto_now_add=True)
+    tipText = models.TextField()
+    TIP_TYPES = [
+        ("quote", "Quote"),
+        ("recommendation", "Recommendation"),
+        ("tip", "Tip"),
+    ]
+    tipType = models.CharField(max_length=20, choices=TIP_TYPES)
+    isActive = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.tipType.title()} for {self.user.username}"
+
+
+# Model for Insights derived from Journal Entries
+class Insight(models.Model):
+    insightID = models.AutoField(primary_key=True)
+    entry = models.ForeignKey(
+        JournalEntry, on_delete=models.CASCADE, related_name="insights"
+    )
+    insightText = models.TextField()
+    sentimentScore = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Insight for Entry {self.entry.entryID}"
