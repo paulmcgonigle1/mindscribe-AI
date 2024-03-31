57eace6e96d7ae7b8dcabdc678643490e5179387
paulmcgonigle <S00210326@atu.ie>
2024-03-31 18:45:51 +0100

    updated to PostGres

--- serverside/serverside/settings.py
+++ serverside/serverside/settings.py
@@ -91,24 +91,24 @@ WSGI_APPLICATION = "serverside.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-# WHEN UPDATING TO POSTGRES
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "",
-#         "USER": "",
-#         "PASSWORD": "",
-#         "HOST": "",
-#         "PORT": "",
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+# WHEN UPDATING TO POSTGRES
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "test",
+        "USER": "postgres",
+        "PASSWORD": "Ilovebaby1",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
