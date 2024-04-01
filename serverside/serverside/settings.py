300e9dc0e4315b4e19eb07d641cb98afa5a18931
paulmcgonigle <S00210326@atu.ie>
2024-04-01 16:42:07 +0100

    updated settings again

--- serverside/serverside/settings.py
+++ serverside/serverside/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "serverside.myapp",
+    "myapp",
     "corsheaders",
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
