173fe57cc60d833db6c6469403c6ebb48ab5750c
paulmcgonigle <S00210326@atu.ie>
2024-04-01 21:52:52 +0100

    updated settings

--- backend/serverside/settings.py
+++ backend/serverside/settings.py
@@ -18,7 +18,7 @@ OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!r&g$3b@3eva_gjgn4cyv50j^9_o&a5ue^(flp!1u*z2#u_ukp"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
