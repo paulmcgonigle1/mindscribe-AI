dddd2e7965eeaa21485d1085f0b43db944a59f5d
paulmcgonigle <S00210326@atu.ie>
2024-04-02 18:36:59 +0100

    added my app urls

--- backend/serverside/urls.py
+++ backend/serverside/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-# from myapp.views import ReactAppView
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("myapp/", include("myapp.urls")),
+    path("myapp/", include("myapp.urls")),
     # path("", ReactAppView.as_view(), name="react-app"),
 ]
