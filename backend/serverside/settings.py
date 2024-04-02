918fc98a74730696fe5c4bfd198189e49a82609c
paulmcgonigle <S00210326@atu.ie>
2024-04-02 17:51:23 +0100

    updatd settings.py again

--- backend/serverside/settings.py
+++ backend/serverside/settings.py
@@ -59,7 +59,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "backend.myapp",
+    "myapp",
     "corsheaders",
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
