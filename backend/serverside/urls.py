3576f01a413a1fd0d29ae1b106ad80979c163c6e
paulmcgonigle <S00210326@atu.ie>
2024-04-02 18:44:02 +0100

    updated urls.py

--- backend/serverside/urls.py
+++ backend/serverside/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("myapp/", include("myapp.urls")),
+    path("myapp/", include("backend.myapp.urls")),
     # path("", ReactAppView.as_view(), name="react-app"),
 ]
