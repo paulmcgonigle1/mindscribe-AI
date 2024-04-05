5515bd34ac0988e15f4d106281816d5556da134d
paulmcgonigle <S00210326@atu.ie>
2024-04-05 18:11:15 +0100

    updated installed app import

--- backend/serverside/settings.py
+++ backend/serverside/settings.py
@@ -64,7 +64,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "corsheaders",
-    "myapp.apps.MyappConfig",
+    "backend.myapp.apps.MyappConfig",
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
 ]
