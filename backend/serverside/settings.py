d854818fd28e96c86cef59b3fc2bda57f6bfb929
paulmcgonigle <S00210326@atu.ie>
2024-04-05 16:39:46 +0100

    commented out myapp

--- backend/serverside/settings.py
+++ backend/serverside/settings.py
@@ -64,7 +64,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "corsheaders",
-    "myapp.apps.MyappConfig",
+    # "myapp.apps.MyappConfig",
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
 ]
