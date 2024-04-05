318dfd1a8638b4130ccafe4ad3317eb1496c909c
paulmcgonigle <S00210326@atu.ie>
2024-04-05 16:20:26 +0100

    updated without myapp

--- backend/serverside/urls.py
+++ backend/serverside/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 
-from myapp.views import ReactAppView
+# from myapp.views import ReactAppView
 
 # updated import
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("myapp/", include("myapp.urls")),
-    re_path(r"^.*$", ReactAppView.as_view(), name="react-app"),
+    # path("myapp/", include("myapp.urls")),
+    # re_path(r"^.*$", ReactAppView.as_view(), name="react-app"),
 ]
