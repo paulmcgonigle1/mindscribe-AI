81db924e77594febc644c205193b6572ff975a1a
paulmcgonigle <S00210326@atu.ie>
2024-04-27 23:29:13 +0100

    fixed backend settings module 'myapp' errors

--- backend/serverside/urls.py
+++ backend/serverside/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 
 from django.urls import path, include, re_path
 
-from myapp.views import ReactAppView
+from backend.myapp.views import ReactAppView
 
 # updated import
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("myapp/", include("myapp.urls")),
+    path("myapp/", include("backend.myapp.urls")),
     re_path(r"^.*$", ReactAppView.as_view(), name="react-app"),
 ]
